77b21d61172ba8692a07d9ec2f535a3f1dd77d1b
Ben Siranosian <bsiranosian@gmail.com>
2022-04-05 15:11:36 -0700

    more cleanup and install work

--- install_cmapR.R
+++ install_cmapR.R
@@ -2,7 +2,6 @@
 if (!require('data.table')) install.packages("data.table",repos = "http://cran.us.r-project.org")
 if (!require('BiocManager')) install.packages("BiocManager",repos = "http://cran.us.r-project.org")
 
-BiocManager::install(version = '3.14', ask=F)
 if (!require('rhdf5')) BiocManager::install(c("rhdf5"),version = "3.14")
 if (!require('prada')) BiocManager::install(c("prada"),version = "3.14")
 if (!require('matrixStats')) BiocManager::install('matrixStats',version = "3.14")
